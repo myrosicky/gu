333e68ddf34c6dc536f3ff8dfc13bff1e21f54df
ll <myrosicky@163.com>
2018-12-20 15:28:07 +0800

    css adjustment

--- src/main/java/org/ll/gn/App.java
+++ src/main/java/org/ll/gn/App.java
@@ -1,7 +1,13 @@
 package org.ll.gn;
 
+import java.util.concurrent.TimeUnit;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -18,9 +24,13 @@ public class App
     	SpringApplication.run(App.class, args);
     }
     
+    private int total = 5;
+    private int current = 0;
+    
     @GetMapping("/")
-    public String index(){
-    	return "index";
+    public String index(HttpServletResponse response){
+    	response.addHeader("Cache-Control", CacheControl.maxAge(0, TimeUnit.SECONDS).noCache().noStore().getHeaderValue());
+    	return "index" + ((current++ ) % total);
     }
     
     
---
+++ src/main/webapp/comm.js
@@ -0,0 +1,114 @@
+WebSpeech.ready(function() {
+    WebSpeech.server = 'http://120.24.87.124/cgi-bin/ekho2.pl';
+    WebSpeech.setVoice('EkhoCantonese');
+  });
+  
+ 
+var end = 100;
+var begin = 0;
+var answer = Math.floor(Math.random() * (end - begin) + begin);
+console.log("answer:" + answer);
+refreshCurrentRange();
+
+function updateBegin(newBegin){
+	begin = newBegin;
+}
+
+function updateEnd(newEnd){
+	end = newEnd;
+}
+
+function refreshCurrentRange(){
+	refreshBeginComponent();
+	refreshEndComponent();
+}
+
+function refreshBeginComponent(){
+	console.log("refreshBeginComponent");
+	var beginObj = document.getElementById("begin");
+	beginObj.className = beginObj.className.replace("rotateActionStyle", "");
+	setTimeout(function(){
+		beginObj.className += " rotateActionStyle";
+		beginObj.innerHTML = begin + "";
+	}, 10);
+	
+}
+
+function refreshEndComponent(){
+	console.log("refreshEndComponent");
+	var endObj = document.getElementById("end");
+	endObj.className = endObj.className.replace("rotateActionStyle", "");
+	setTimeout(function(){
+		endObj.className += " rotateActionStyle";
+		endObj.innerHTML = end;
+	}, 10);
+}
+
+function speakCurrentRange(callBackFunc){
+		WebSpeech.speak(begin + "");
+		WebSpeech.onfinish = function () { 
+			WebSpeech.speak("至");
+			WebSpeech.onfinish = function () { 
+				WebSpeech.speak(end + "");
+				WebSpeech.onfinish = function () {
+					callBackFunc();
+					WebSpeech.onfinish = function (){};
+				};
+			};
+		};
+}
+
+function displayBingoStyle(){
+	var effectOn = false;
+	var beginStyle = document.getElementById("begin").className;
+	var endStyle = document.getElementById("end").className;
+	
+	setInterval(function(){
+		if(effectOn){
+			beginStyle = beginStyle.replace("bingoNumberStyle", ' ');
+			endStyle = endStyle.replace("bingoNumberStyle", ' ');
+			effectOn = false;
+		}else{
+			beginStyle += " bingoNumberStyle" ;
+			endStyle += " bingoNumberStyle" ;
+			effectOn = true;
+		}
+		document.getElementById("begin").className = beginStyle;
+		document.getElementById("end").className = endStyle;
+	}, 1000);
+	
+}
+
+var enterProcessing = false;
+function enter(){
+	if(enterProcessing){
+		return;
+	}else{
+		enterProcessing = true;
+	}
+	var speakNum = parseInt(document.getElementById("speakNum").value.trim(), 10);
+	setTimeout(function(){
+		if(speakNum == answer){ // answer hit
+			updateBegin(speakNum);
+			updateEnd(speakNum);
+			refreshCurrentRange();
+			var player = videojs('my-player');
+			setTimeout(function(){
+				displayBingoStyle();
+				player.play();
+			}, 500);
+		}else{										// update range
+			if(speakNum > answer && speakNum < end){
+				updateEnd(speakNum);
+				speakCurrentRange(refreshEndComponent);
+			}else if(speakNum < answer && speakNum > begin){
+				updateBegin(speakNum);
+				speakCurrentRange(refreshBeginComponent);
+			}else{
+				WebSpeech.speak("数字必须在范围内");
+			}
+		}
+		document.getElementById("speakNum").value = "";
+		enterProcessing = false;
+	}, 1000);
+}
