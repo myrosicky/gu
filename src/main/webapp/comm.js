9105a329a76f9b1f0e9e46a7ffceb16901de4bec
ll <myrosicky@163.com>
2018-12-22 09:13:34 +0800

    update style

--- src/main/webapp/comm.js
+++ src/main/webapp/comm.js
@@ -6,7 +6,7 @@ WebSpeech.ready(function() {
  
 var end = 100;
 var begin = 0;
-var answer = Math.floor(Math.random() * (end - begin) + begin);
+var answer = Math.floor(Math.random() * ((end-1) - (begin+1)) + (begin+1));
 console.log("answer:" + answer);
 refreshCurrentRange();
 
@@ -77,6 +77,30 @@ function displayBingoStyle(){
 	
 }
 
+function mute(isMute){
+	var player = videojs('my-player');
+	player.ready(function() {
+		player.muted(isMute);
+	});
+}
+
+function playHeartbeat(callBackFunc){
+	var player = videojs('my-player');
+	var originalSrc = player.src();
+	var originalVolume = player.volume();
+	player.src('http://img.tukuppt.com/preview_music/00/03/15/preview-5b835c9543dfb4039.mp3');
+	player.volume(30); 
+	player.ready(function() {
+		player.play();
+		setTimeout(function(){
+			player.pause();
+			player.src(originalSrc);
+			player.volume(originalVolume); 
+			callBackFunc();
+		}, 4000);
+	});
+}
+
 var enterProcessing = false;
 function enter(){
 	if(enterProcessing){
@@ -90,8 +114,7 @@ function enter(){
 		return;
 	}
 	var speakNum = parseInt(text, 10);
-	
-	setTimeout(function(){
+	playHeartbeat(function(){
 		if(speakNum == answer){ // answer hit
 			updateBegin(speakNum);
 			updateEnd(speakNum);
@@ -99,7 +122,10 @@ function enter(){
 			var player = videojs('my-player');
 			setTimeout(function(){
 				displayBingoStyle();
-				player.play();
+				player.ready(function() {
+					player.volume(0.5); 
+					player.play();
+				});
 			}, 500);
 		}else{										// update range
 			if(speakNum > answer && speakNum < end){
@@ -114,5 +140,5 @@ function enter(){
 		}
 		document.getElementById("speakNum").value = "";
 		enterProcessing = false;
-	}, 1000);
+	});
 }
